c4bb1a08924f6aa8689c930b26e74f8e9645a32d
Larryinx <1155160255@link.cuhk.edu.hK>
2024-02-21 02:12:12 +0800

    test image

--- web/index.php
+++ web/index.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Example PHP page for CSCI4140 24SP</title>
-  </head>
-  <body>
-    <?php
-      echo '<h1>Hello world!</h1>';
-      echo '<p>This page uses PHP version '
-          . phpversion()
-          . '.</p>';
-      include('db_connect.php');
-    ?>
-  </body>
+
+<head>
+  <title>Example PHP page for CSCI4140 24SP</title>
+</head>
+
+<body>
+  <!-- <?php
+  echo '<h1>Hello world!</h1>';
+  echo '<p>This page uses PHP version '
+    . phpversion()
+    . '.</p>';
+  include('db_connect.php');
+  ?> -->
+  <?php
+  $image = new Imagick();
+  $image->newImage(100, 100, new ImagickPixel('red'));
+  $image->setImageFormat('png');
+
+  header('Content-Type: image/png');
+  echo $image;
+  ?>
+</body>
+
 </html>
