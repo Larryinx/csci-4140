7a175576c3337b0599a1bfa5b6ec672a5d200800
Larryinx <1155160255@link.cuhk.edu.hK>
2024-02-21 00:24:40 +0800

    set up database

--- web/db_connect.php
+++ web/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'dpg-cm7a80mdxxxxxxxxxx';
-$dbname = 'demo_xxxxxxxx';
-$username = 'xxxxxxx';
-$password = 'ecMTqrDov1NDxxxxxxxxxxx';
+$host = 'dpg-cnabbf6n7f5s73el8v3g-a';
+$dbname = 'csci4140assignment1';
+$username = 'csci4140assignment1_user';
+$password = 'XU7wmFLzWij8XPeYQaJXfGCfAKmV05Md';
 
 try {
     $conn = new PDO("pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password");
